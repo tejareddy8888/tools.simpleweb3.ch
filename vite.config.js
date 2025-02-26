6063eaee890cb179dc86c580e7bfc6fe7624f8d8
tejareddy8888 <saitejapottanigari@gmail.com>
2025-02-26 20:29:32 +0100

    added lazy load

--- vite.config.js
+++ vite.config.js
@@ -12,15 +12,5 @@ export default defineConfig({
   envPrefix: 'TOOLS_',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          ui: ['@emotion/react', '@emotion/styled', '@mui/material', '@headlessui/react'],
-          web3: ['@rainbow-me/rainbowkit', 'viem', 'wagmi'],
-          state: ['@reduxjs/toolkit', 'react-redux'],
-        },
-      },
-    },
   }
 })
