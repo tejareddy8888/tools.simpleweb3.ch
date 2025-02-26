d6982e6d354d0f1aa5e739db6765406dc1404140
tejareddy8888 <saitejapottanigari@gmail.com>
2025-02-26 07:33:35 +0100

    improved the dockerize

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: true,
     strictPort: true,
     port: 4173
   },
-  envPrefix: 'TOOLS_'
+  envPrefix: 'TOOLS_',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@emotion/react', '@emotion/styled', '@mui/material', '@headlessui/react'],
+          web3: ['@rainbow-me/rainbowkit', 'viem', 'wagmi'],
+          state: ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  }
 })
