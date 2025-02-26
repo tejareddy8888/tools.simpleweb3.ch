b6775e37316e281144db4a576d23cefbd9dd73e2
tejareddy8888 <saitejapottanigari@gmail.com>
2025-02-26 21:29:59 +0100

    improved the dockerization

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,10 @@ export default defineConfig({
   envPrefix: 'TOOLS_',
   build: {
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
-  }
+  },
+  preview: {
+    host: true,
+    strictPort: true,
+    port: 4173
+  },
 })
