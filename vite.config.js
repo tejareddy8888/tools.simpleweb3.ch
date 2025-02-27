77153fc818603aa56cc8ebdb6749a676e34f6d6e
tejareddy8888 <saitejapottanigari@gmail.com>
2025-02-27 19:42:48 +0100

    imprved building the for production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true,
     strictPort: true,
@@ -11,6 +12,8 @@ export default defineConfig({
   },
   envPrefix: 'TOOLS_',
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
   },
   preview: {
